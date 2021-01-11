afcecab169cd08cef1e966ef81270f55ca7af0d6
Kenny Goff <goff.kenneth@gmail.com>
2021-01-10 21:02:09 -0500

    Add label filtering to Forge

--- src/feint/forge/Forge.hx
+++ src/feint/forge/Forge.hx
@@ -12,6 +12,8 @@ class Forge {
   var components:Map<ComponentType, Array<Component>> = [];
   var systems:Array<System> = [];
   var renderSystems:Array<RenderSystem> = [];
+  var labelMap:Map<String, Array<EntityId>> = [];
+  var entityLabels:Map<EntityId, Array<String>> = [];
 
   public var map:Map<EntityId, Array<Component>> = [];
 
@@ -33,8 +35,9 @@ class Forge {
    * Add entity by id with given components
    * @param id Entity id
    * @param comps Array of components registered to this Entity
+   * @param labels (Optional) Array of labels for filtering in systems
    */
-  public function addEntity(id:EntityId, comps:Array<Component>) {
+  public function addEntity(id:EntityId, comps:Array<Component>, ?labels:Array<String>) {
     entities.push(id);
     map[id] = [];
     for (component in comps) {
@@ -45,16 +48,39 @@ class Forge {
       components[Type.getClassName(Type.getClass(component))].push(component);
       map[id].push(component);
     }
+
+    if (labels != null) {
+      for (label in labels) {
+        if (this.labelMap[label] == null) {
+          this.labelMap[label] = [];
+        }
+
+        this.labelMap[label].push(id);
+      }
+      this.entityLabels[id] = labels.copy();
+    } else {
+      this.entityLabels[id] = [];
+    }
   }
 
   /**
    * Get EntityIds for all entities that have all listed components
    * @param componentClassNames Class names of components to check for
+   * @param labels (Optional) Array of labels to filter entities by
    * @return Array<EntityId>
    */
-  public function getEntities(componentClassNames:Array<ComponentType>):Array<EntityId> {
+  public function getEntities(
+    componentClassNames:Array<ComponentType>,
+    ?labels:Array<String>
+  ):Array<EntityId> {
     if (componentClassNames.length == 1) {
-      return components[componentClassNames[0]].map(component -> component.entity);
+      var matchingEntities = components[componentClassNames[0]].map(component -> component.entity);
+      if (labels != null) {
+        matchingEntities = matchingEntities.filter(
+          entityId -> labels.foreach(label -> entityLabels[entityId].contains(label))
+        );
+      }
+      return matchingEntities;
     }
 
     var compArr:Array<Array<Component>> = componentClassNames.filter(
@@ -62,13 +88,21 @@ class Forge {
     )
       .map(componentClassName -> components[componentClassName]);
 
-    return compArr.fold((components, jointEntities) -> {
+    var matchingEntities = compArr.fold((components, jointEntities) -> {
       var componentEntities = components.map(component -> component.entity);
       if (jointEntities == null) {
         return componentEntities;
       }
       return componentEntities.filter(id -> jointEntities.contains(id));
     }, null);
+
+    if (labels != null) {
+      matchingEntities = matchingEntities.filter(
+        entityId -> labels.foreach(label -> entityLabels[entityId].contains(label))
+      );
+    }
+
+    return matchingEntities;
   }
 
   public function getEntityComponent<T:Component>(entityId:EntityId, componentType:Class<T>):T {
